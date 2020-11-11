d2b49ebbca0b6a2613ad021ffd5f8a43c74a1bf5
Hoang <githubblackpanter99@gmail.com>
2020-11-11 21:01:34 +0700

    update db

--- sql/db_authentication.sql
+++ sql/db_authentication.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Oct 25, 2020 at 06:16 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.3.18
+-- Máy chủ: localhost
+-- Thời gian đã tạo: Th10 11, 2020 lúc 03:00 PM
+-- Phiên bản máy phục vụ: 10.3.16-MariaDB
+-- Phiên bản PHP: 7.2.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_authentication`
+-- Cơ sở dữ liệu: `db_authentication`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `group_name`
+-- Cấu trúc bảng cho bảng `group_name`
 --
 
 CREATE TABLE `group_name` (
@@ -32,10 +33,40 @@ CREATE TABLE `group_name` (
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Đang đổ dữ liệu cho bảng `group_name`
+--
+
+INSERT INTO `group_name` (`id`, `name`) VALUES
+(1, 'group1'),
+(2, 'group2'),
+(3, 'group3'),
+(4, 'group4');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Cấu trúc bảng cho bảng `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `name_role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `role`
+--
+
+INSERT INTO `role` (`id`, `name_role`) VALUES
+(1, 'ROLE_SUPERUSER'),
+(2, 'ROLE_ADMIN'),
+(3, 'ROLE_USER');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
 --
 
 CREATE TABLE `user` (
@@ -46,62 +77,96 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`id`, `mssv`, `name`, `email`, `password`) VALUES
+(1, '17020766', 'Nguyen Canh Hoang', '17020766@gmail.com', '$2y$12$MpLd3N3kj3/nlTfSJDo0Qeh3e7F80anAFpeqcsOWy.Ny3iXp8.YkC'),
+(2, '17020767', 'Hoang nguyen', 'canhhoanght99@gmail.com', '$2y$12$MpLd3N3kj3/nlTfSJDo0Qeh3e7F80anAFpeqcsOWy.Ny3iXp8.YkC'),
+(3, '17020767', 'Hoang nguyen', 'canhhoanght999@gmail.com', '$2a$10$GtnmcU1hv4P/fGjh2U39FeuujN8m61FRHsI8IsSJnC5Ucx.oX1SlO'),
+(4, '17020712', 'Hoang ngxxuyen', 'canhhoasadasnght9xx99@gmail.com', '$2a$10$g4IthSb2cgZjyrtkrfR4WuOMU26ixPf7cYdNM4ifDNLdM7/qYCwcm');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_role`
+-- Cấu trúc bảng cho bảng `user_role`
 --
 
 CREATE TABLE `user_role` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
-  `role` varchar(45) NOT NULL
+  `role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Đang đổ dữ liệu cho bảng `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `user_id`, `group_id`, `role_id`) VALUES
+(4, 1, 1, 1),
+(5, 1, 2, 2),
+(6, 1, 3, 3),
+(7, 1, 4, 2),
+(8, 2, 4, 2),
+(9, 3, 4, 2);
+
+--
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `group_name`
+-- Chỉ mục cho bảng `group_name`
 --
 ALTER TABLE `group_name`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- Chỉ mục cho bảng `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user_role`
+-- Chỉ mục cho bảng `user_role`
 --
 ALTER TABLE `user_role`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `group_name`
+-- AUTO_INCREMENT cho bảng `group_name`
 --
 ALTER TABLE `group_name`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT cho bảng `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `user_role`
+-- AUTO_INCREMENT cho bảng `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
